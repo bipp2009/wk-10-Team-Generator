542bc7526b4d0fd8dd2f78817ed5db3e576efb81
Bipin <bipp2009@gmail.com>
2021-05-07 19:06:47 +0100

    crreated file structure and package.json

---
+++ index.js
@@ -0,0 +1,7 @@
+/*Declare the variables used in the node index file: inquirer used to generate questions, fs to reference file system, utils to ref the utilities used, 
+and team generator to use the folder file
+*/
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require('util');
+const generatehtml = require("./utils/generatehtml");
