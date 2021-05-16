f02b606fdd524204359104b8234e8bc0f3b47b5b
Bipin <bipp2009@gmail.com>
2021-05-16 15:08:09 +0100

    Added manager creation functionality

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require('util');
-const generatehtml = require("./utils/generatehtml");
+const path = require("path")
+const randerHtml = require("./utils/generatehtml");
+const Manager = require("./classes/manager");
+
+const output_dir = path.resolve(__dirname, "output");
+const output_path = path.join(output_dir, "team.html");
+
+const teamArray = [];
+
+function init() {
+
+    function managerCreation() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the managers name?"
+            }, 
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the managers id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the managers email?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the managers office number?"
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber); 
+            teamArray.push(manager);
+            console.log(teamArray);
+            teamCreation();
+        })
+    }
+
+ function teamCreation(){
+
+ }
+
+ function engineerCreation() {
+
+ }
+
+ function internCreation(){
+
+ }
+
+ function generateHtmlPage(){
+
+ }
+
+ managerCreation();
+}
 
 
-
+init();
 
