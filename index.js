02e78670c722c5e7cff19e8ee1b5b10ecb6246dd
Bipin <bipp2009@gmail.com>
2021-05-16 15:11:27 +0100

    Added functionality for getting users next move

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
-const util = require('util');
-const path = require("path")
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
 const randerHtml = require("./utils/generatehtml");
 const Manager = require("./classes/manager");
 
@@ -11,56 +11,71 @@ const output_path = path.join(output_dir, "team.html");
 const teamArray = [];
 
 function init() {
+  function managerCreation() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the managers name?",
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the managers id?",
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the managers email?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the managers office number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.officeNumber
+        );
+        teamArray.push(manager);
+        console.log(teamArray);
+        teamCreation();
+      });
+  }
 
-    function managerCreation() {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "managerName",
-                message: "What is the managers name?"
-            }, 
-            {
-                type: "input",
-                name: "managerId",
-                message: "What is the managers id?"
-            },
-            {
-                type: "input",
-                name: "managerEmail",
-                message: "What is the managers email?"
-            },
-            {
-                type: "input",
-                name: "officeNumber",
-                message: "What is the managers office number?"
-            }
-        ]).then(answers => {
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber); 
-            teamArray.push(manager);
-            console.log(teamArray);
-            teamCreation();
-        })
-    }
+  function teamCreation() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "nextMove",
+          message: "Would you like to add any more employees?",
+          choices: ["Add an Engineer", "Add an Intern", "I'm finished"],
+        },
+      ])
+      .then((answer) => {
+        if (answer.nextMove === "Add an Engineer") {
+          engineerCreation();
+        } else if (answer.nextMove === "Add in Intern") {
+          internCreation();
+        } else {
+          generateHtmlPage();
+        }
+      });
+  }
 
- function teamCreation(){
+  function engineerCreation() {}
 
- }
+  function internCreation() {}
 
- function engineerCreation() {
+  function generateHtmlPage() {}
 
- }
-
- function internCreation(){
-
- }
-
- function generateHtmlPage(){
-
- }
-
- managerCreation();
+  managerCreation();
 }
 
-
 init();
-
